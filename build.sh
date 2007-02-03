42dd9859593d47dcff5a8c2f7f6c9cc311fde49d
moseley <moseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2007-02-03 01:00:11 +0000

    Initial updates for moving website to new machine. Just a few more tweaks, shell scripts and httpd.conf and we can have a beer.  No, wait, beer first.

--- build.sh
+++ build.sh
@@ -1,38 +1,74 @@
 #!/bin/bash
 
 # Rebuild the site, and if any files are written reindex
+# This is expected to be run as a cron job
+# cd to the swish_website dir then run this script
+
+
+# Directory where everything happens
+WEB_ROOT="$(pwd)/.."
+
+
+# Path to the archive index file -- so search script can find the index.
+ARCHIVE_INDEX="$WEB_ROOT/swish/search/index.swish-e"
+
+# Path to swish-e source to find pods for building the docs
+SWISH_SRC="$WEB_ROOT/swish_src"
+
+# Path to the daily build soruce to build the dev docs
+DEV_SRC="$WEB_ROOT/devel_src"
+
 
 SWISH_SITE=http://swish-e.org
 SPIDER_QUIET=1
 export SWISH_SITE SPIDER_QUIET
 
-TMP=$HOME/swish_website.$$.tmp
+
+# Where to store output from svn 
+TMP=tmp/swish_website.$$.tmp
 
 
-if ! cvs -q update &>$TMP; then
-    echo "cvs update failed"
+# First try and do a svn update
+
+if ! svn update &>$TMP; then
+    echo "svn update failed"
     echo
     cat $TMP; rm $TMP
     exit
 fi
 
 # Look out for any local problems
+#    A  Added
+#    D  Deleted
+#    U  Updated
+#    C  Conflict
+#    G  Merged
 
-if egrep '^(M|C|A|R) ' $TMP >/dev/null; then
+# Check for conflicts -- which should not happen
+# unless messing with the source locally
+
+if egrep '^C ' $TMP >/dev/null; then
     echo "Some files need attention"
     echo
     cat $TMP
     rm  $TMP
     exit
-fi 
-
-
-if [ -n "$(echo $@ | grep -- '-a')"  ] || egrep '^(U|P) ' $TMP >/dev/null; then
-    echo "updating site"
-    perl bin/build \
-        -archive=$HOME/swish/search/index.swish-e \
-        -swishsrc=swishsrc \
-        -develsrc=develsrc $@ && swish-e -c etc/swish.config -S prog -v 0
 fi
+
+
+# Now build the website, if any -a passed or if "Updated to revision" is returned.
+# Or perhaps could just check for (A|C|D|U|G)
+
+if [ -n "$(echo $@ | grep -- '-a')"  ] || egrep '^Updated to revision' $TMP >/dev/null; then
+    echo "updating site"
+    bin/build \
+        -archive=$ARCHIVE_INDEX \
+        -swishsrc=$SWISH_SRC \
+        -develsrc=$DEV_SRC \
+        $@ \
+    && swish-e -c etc/swish.config -S prog -v 0
+fi
+
+
 
 rm $TMP

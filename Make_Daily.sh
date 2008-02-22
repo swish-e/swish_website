45d77494147fdf3332f914769d194d943e841b61
joshr <joshr@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2008-02-22 13:44:45 +0000

    added code to build the 2.6/2.7 branch too, and also to use --no-symlink and new --no-latest options to swish-daily.pl so that symlinks are not created for the 2.6/2.7 branch tarball/dir.

--- Make_Daily.sh
+++ Make_Daily.sh
@@ -2,8 +2,21 @@
 
 DIR="${BASE_DIR:=$HOME/swish}"
 
-
+############################################ 
+# build the 2.4 branch
 swish-daily.pl \
+    --topdir=$DIR/swish_daily_build \
+    --tardir=$DIR/swish-daily || exit 1;
+
+
+############################################ 
+# build the 2.6 branch (which makes 2.7.0 now)
+# --no-latest  avoids making latest.tar.gz      symlink. 
+# --no-symlink avoids making latest_swish_build symlink
+swish-daily.pl \
+    --svnco='svn co http://svn.swish-e.org/swish-e/branches/2.6/' \
+    --no-latest \
+    --no-symlink \
     --topdir=$DIR/swish_daily_build \
     --tardir=$DIR/swish-daily || exit 1;
 
